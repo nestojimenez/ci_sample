35b67912181182ebe4db66318cf717f5cbefb6a1
nestojimenez <aguiladescalza@gmail.com>
2023-07-28 09:55:18 -0700

    Esling fixed

---
+++ .eslintrc.js
@@ -0,0 +1,54 @@
+module.exports = {
+    "env": {
+      "browser": true,
+      "es6": true,
+      "jest/globals": true,
+      "node": true
+    },
+    "extends": [
+      "eslint:recommended",
+      "plugin:react/recommended"
+    ],
+    "parserOptions": {
+      "ecmaFeatures": {
+        "jsx": true
+      },
+      "ecmaVersion": 2018,
+      "sourceType": "module",
+      "env":{
+        "es6": true
+      }
+    },
+    "plugins": [
+      "react", "jest"
+    ],
+    "rules": {
+      "indent": [
+        "error",
+          2
+        ],
+      "no-extra-semi": ["warn"],
+      "linebreak-style": [
+        "error",
+        "windows"
+      ],
+      "quotes": [
+        "error",
+        "single"
+      ],
+      "semi": [
+        "error",
+        "never"
+      ],
+      "eqeqeq": "error",
+      "no-trailing-spaces": "error",
+      "object-curly-spacing": [
+        "error", "always"
+      ],
+      "arrow-spacing": [
+        "error", { "before": true, "after": true }
+      ],
+      "no-console": "warn",
+      "react/prop-types": 0
+    }
+  }
